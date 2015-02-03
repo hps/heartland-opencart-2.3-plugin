b10a5162b950d935ca7f113dd3a8216a2df9a429
Shane Logsdon <shane.logsdon@e-hps.com>
2015-02-03 12:26:59 -0500

    HpsChargeService -> HpsCreditService. fix deprecation warning

--- catalog/controller/payment/securesubmitfiles/services/HpsChargeService.php
+++ catalog/controller/payment/securesubmitfiles/services/HpsChargeService.php
@@ -2,7 +2,8 @@
 
 class HpsChargeService extends HpsCreditService {
     function __construct(HpsConfiguration $config=null){
-        error_log('HpsChargeService is Deprecated and will soon be removed.', E_USER_DEPRECATED);
+        $flag = defined('E_USER_DEPRECATED') ? E_USER_DEPRECATED : E_USER_NOTICE;
+        error_log('HpsChargeService is Deprecated and will soon be removed.', $flag);
         parent::__construct($config);
     }
 } 
