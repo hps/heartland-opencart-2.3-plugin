b5c167330138d4fa6871689e0bbefbbc212976a4
charles.simmons@e-hps.com <charles.simmons@e-hps.com>
2016-06-15 17:49:04 -0400

    Added velocity checks and updated to latest php-SDK

--- catalog/controller/payment/securesubmitfiles/services/HpsChargeService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HpsChargeService extends HpsCreditService {
-    function __construct(HpsConfiguration $config=null){
-        $flag = defined('E_USER_DEPRECATED') ? E_USER_DEPRECATED : E_USER_NOTICE;
-        error_log('HpsChargeService is Deprecated and will soon be removed.', $flag);
-        parent::__construct($config);
-    }
-} 
