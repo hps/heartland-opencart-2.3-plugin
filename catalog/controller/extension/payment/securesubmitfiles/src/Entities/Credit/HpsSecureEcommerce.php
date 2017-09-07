8ff8f0f503c1ffedd59ae0bd9553fdfc53f46c7c
Senthil Kumar Muthusamy <sen.kumar2007@gmail.com>
2017-09-07 09:10:09 -0400

    SDK updates

---
+++ catalog/controller/extension/payment/securesubmitfiles/src/Entities/Credit/HpsSecureEcommerce.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class HpsSecureEcommerce
+ */
+class HpsSecureEcommerce
+{
+    public $dataSource = null;
+    public $type       = null;
+    public $data       = null;
+    public $eciFlag    = null;
+    public $xid        = null;
+}
