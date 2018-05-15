b3555a89cd7f13bc3774e37cfe6923ea424fc62d
jeevitha90 <jeevaa.yuvaa@gmail.com>
2018-05-15 06:58:01 -0400

    Upgrade Version 3.x 1. Throwing Error while configuration: Undefined index: token 2. Undefined file .tpl 3. Adding prefix in field name 4. Version number change

--- catalog/model/extension/payment/securesubmit.php
+++ catalog/model/extension/payment/securesubmit.php
@@ -2,12 +2,14 @@
 class ModelExtensionPaymentSecuresubmit extends Model {
   	public function getMethod($address, $total) {
 		$this->load->language('extension/payment/securesubmit');
+		
+		$prefix = version_compare(VERSION, '3.0.0', '>=') ? 'payment_' : '';
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('securesubmit_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get($prefix . 'securesubmit_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
-		if ($this->config->get('securesubmit_total') > 0 && $this->config->get('securesubmit_total') > $total) {
+		if ($this->config->get($prefix . 'securesubmit_total') > 0 && $this->config->get($prefix . 'securesubmit_total') > $total) {
 			$status = false;
-		} elseif (!$this->config->get('securesubmit_geo_zone_id')) {
+		} elseif (!$this->config->get($prefix . 'securesubmit_geo_zone_id')) {
 			$status = true;
 		} elseif ($query->num_rows) {
 			$status = true;
@@ -22,7 +24,7 @@ class ModelExtensionPaymentSecuresubmit extends Model {
 				'code'       => 'securesubmit',
 				'title'      => $this->language->get('text_title'),
 				'terms'		 => '',
-				'sort_order' => $this->config->get('securesubmit_sort_order')
+				'sort_order' => $this->config->get($prefix . 'securesubmit_sort_order')
 			);
 		}
 
